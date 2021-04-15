be8685b112688a44b784696a529604660d0c8008
Victor Leite Salvetti <victorsalvetti@hotmail.com>
2021-04-15 19:59:27 -0300

    created main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,14 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'main',
+    loadChildren: () => import('./main/main.module').then( m => m.MainPageModule)
+  },
+  {
+    path: 'pet-register',
+    loadChildren: () => import('./pet-register/pet-register.module').then( m => m.PetRegisterPageModule)
+  },
 
 ];
 
