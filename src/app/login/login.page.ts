6a9c449ce34d24e0f5607378aa39ef7f1d521fdc
JoaoJuliasz <joaojuliaszdemorais@gmail.com>
2021-04-26 20:30:11 -0300

    add login eye validation and profile variables

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  public eyeType: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.eyeType = '-off-outline';
+  }
+
+  changeEyeType = function() {
+    console.warn('entrei')
+    this.eyeType = this.eyeType === '-off-outline' ? '' : '-off-outline';
   }
 
 }
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  public profileName: string;
+  public profileIdentity: string;
+  public profileMail: string;
+  public profileAddress: string;
+
+
   constructor() { }
 
   ngOnInit() {
